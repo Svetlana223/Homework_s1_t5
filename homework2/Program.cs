c085619549c27b40cc321ffba755082a399a809f
Svetlana <strel_sv@bk.ru>
2022-04-15 19:26:05 +0300

    программу, которая на вход принимает два числа и выдаёт, какое число большеe

---
+++ homework2/Program.cs
@@ -0,0 +1,17 @@
+﻿//Напишите программу, которая на вход
+//принимает два числа и выдаёт, какое число большее, а
+// какое меньшее. 
+
+Console.WriteLine("Введите 1 число: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите 2 число: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+if(number1 > number2)
+{
+    Console.WriteLine(number1 + " больше " + number2);
+}
+else
+{
+    Console.WriteLine(number1 + " меньше " + number2);
+}
