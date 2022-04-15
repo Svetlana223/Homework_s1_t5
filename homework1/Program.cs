45d9b6160ee42f7d256ce3f1ec8ec4d0f8f49b41
Svetlana <strel_sv@bk.ru>
2022-04-15 19:23:53 +0300

    программa, которая принимает на вход трёхзначное число и на выходе показывает последнюю цифру этого числa

---
+++ homework1/Program.cs
@@ -0,0 +1,6 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число и 
+//на выходе показывает последнюю цифру этого числа. 456 -> 6 782 -> 2 918 -> 8
+int number1 = new Random().Next(100,1000);
+Console.WriteLine(number1);
+int number2 = number % 10;
+Console.WriteLine(number2);
